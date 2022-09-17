982fd0014471f140389326d7eaaf47da959a66b4
Kamal Walia <kamalw1709@gmail.com>
2022-09-17 11:33:59 +0530

    Redux Thunk

--- src/store/actions/count.js
+++ src/store/actions/count.js
@@ -1,9 +1,16 @@
 import Actions from "."
+import axios from "axios";
 
-export const incrementCount = (count) => {
-    return {
-        type: Actions.incrementCount,
-        payload: count
+export const incrementCount =  (count) => {
+    return async (dispatch, getState) => {
+        const countAsId = getState().count
+        await axios.get(`https://jsonplaceholder.typicode.com/todos/${countAsId}`).then((res) => {
+            console.log("Data", res.data)
+        })
+        dispatch({
+            type: Actions.incrementCount,
+            payload: count
+        })
     }
 }
 
